de0b21891f0685c2127ab0ef4095389f794a147e
thilangaj <thilangajayamanne@gmail.com>
2015-08-03 11:04:24 +0530

    customer service

---
+++ ChannelSystem/sriMedicalSystem/src/main/java/lk/ac/ucsc/webArc/assgnment/ChannelingSystem/service/CustomerService.java
@@ -0,0 +1,11 @@
+package lk.ac.ucsc.webArc.assgnment.ChannelingSystem.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class CustomerService {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+}
